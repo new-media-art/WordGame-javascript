ca143f4a471fbf2c8a82008ba4f3b467ba95c3b6
Ulrico Carmona <ulricocarmona@gmail.com>
2022-04-25 22:42:14 +0200

    finished click handle for keyboard

---
+++ app.js
@@ -0,0 +1,69 @@
+const tileDisplay = document.querySelector('.tile-container');
+const keyboard = document.querySelector('.key-container');
+
+const wordle = 'SUPER'
+const keys = [
+    'Q',
+    'W',
+    'E',
+    'R',
+    'T',
+    'Y',
+    'U',
+    'I',
+    'O',
+    'P',
+    'A',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'ENTER',
+    'Z',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N',
+    'M',
+    '«',
+];
+
+
+
+const guessRows = [
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+]
+
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+    guessRow.forEach((_guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement)
+    })
+    tileDisplay.append(rowElement)
+})
+
+keys.forEach(key => {
+   const buttonElement = document.createElement('button')
+   buttonElement.textContent = key
+   buttonElement.setAttribute('id', key)
+   buttonElement.addEventListener('click', () => handleClick(key))
+   keyboard.append(buttonElement)
+})
+
+const handleClick = (key) => {
+    console.log('clicked', key)
+}
